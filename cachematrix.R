579e0a5fbf044aa5f5848c8903f3f4ff56eb65c7
Balaji Kesavan <balaji.kesavan@aggiemail.usu.edu>
2015-08-23 11:01:14 -0500

    edited cacheMatrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,48 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This function inverses a matrix and then caches the value for future use
 
-## Write a short comment describing this function
+#creating a function called makeCacheMatrix and setting the default value of its input as an empty matrix. This is output a list containing various functions 
+makeCacheMatrix <- function(x = matrix()){
 
-makeCacheMatrix <- function(x = matrix()) {
+#creating a variable called temp and setting it NULL. This variable will store the value the cached matrix
+  temp <- NULL
 
+#creating a function called set, which will be re-assign the variable temp to NULL if a different vector is passed in the original function
+  set <- function(y){
+    x <<- y
+    temp <<- NULL
+  }
+
+#creating a function called get, which will return the matrix originally entered, this is only if the matric entered is different from the matrix whose inverse is cached
+  get <- function()x
+  
+#creating a function called SetInverse, which will Set the value of the temp to the cached inverse
+  SetInverse <- function(inverse)temp <<- inverse
+  
+#creating a function called GetInverse, that will return the value of temp  
+  GetInverse <- function()temp
+  
+#Creating a special vector, containing the 4 functions we just created  
+  list(set=set, get=get, SetInverse=SetInverse, GetInverse=GetInverse)
 }
 
 
-## Write a short comment describing this function
+#creating a function called Cache Solve that will input, the output of the MakeCacheMatrix
+CacheSolve <- function(x, ...){
+  
+#extracting the value of temp using the GetInverse function
+  temp <- x$GetInverse()
+  
+#if the value of temp is available then the cached value and a relevant message is displayed and the function stops running
+  if(!is.null(temp)) {
+    message("this is cached info")
+    return(temp)
+  }
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+#if the value of temp is not available then the Inverse is calculated using the Solve function
+else{
+  data <- x$get()
+  temp <- solve(data)
+  x$SetInverse(temp)
+  temp
+  }
 }
